49fe0dad9c230dd75210230f9f30e08c4871a8ed
AngelOfRadicalWisdom <31025996+AngelOfRadicalWisdom@users.noreply.github.com>
2017-08-29 12:31:26 +0800

    StudentsComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+import { InsertStudentsComponent } from './insert-students/insert-students.component';
+import { PrintingComponent } from './printing/printing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent,
+    InsertStudentsComponent,
+    PrintingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
